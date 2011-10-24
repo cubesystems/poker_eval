359b12ebc45b83d11f0d66451ba98468e3a589bd
miks <miks@cubesystems.lv>
2011-10-25 01:27:53 +0800

    Implement winners method

--- lib/poker_eval.rb
+++ lib/poker_eval.rb
@@ -8,5 +8,58 @@ class PokerEval
   module GemVersion
     VERSION = '0.0.1'
   end
-end
 
+  def self.winner args
+    index2index = {}
+    normalized_pockets = []
+    normalized_index = 0
+    pockets = args["pockets"]
+
+    pockets.each_with_index do |pocket, index|
+      if !args["fill_pockets"]
+        if pocket.find_index("__")
+          pockets[index] = []
+        end
+      end
+
+      if !pockets[index].empty?
+        normalized_pockets << pockets[index]
+        index2index[index] = normalized_index
+        normalized_index += 1
+      end
+    end
+
+    args["pockets"] = normalized_pockets
+    results = self.eval(args)
+
+
+    count = results["info"]["samples"]
+    haslopot = results["info"]["haslopot"]
+    hashipot = results["info"]["hashipot"]
+
+    winners = { 'low' => [], 'hi' => [] }
+
+    pockets.each_with_index do |pocket, index|
+      if !index2index[index].nil?
+        result = results["eval"][index2index[index]]
+        if result["winhi"] == 1 or result["tiehi"] == 1
+            winners["hi"] << index
+        end
+        if result["winlo"] == 1 || result["tielo"] == 1
+          winners["low"] << index
+        end
+      end
+    end
+
+    if !haslopot || winners["low"].empty?
+      winners.delete("low")
+    end
+
+    if !hashipot
+      winners.delete("hi")
+    end
+
+    return winners
+
+  end
+end
