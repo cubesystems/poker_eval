4450d0a6c0b6a94e05aad5e6be2fbbb567c9d3fa
miks <miks@cubesystems.lv>
2011-10-24 17:57:19 +0300

    test

--- test/test_poker-eval.rb
+++ test/test_poker-eval.rb
@@ -4,3 +4,16 @@
 require "test/unit"
 require 'ostruct'
 
+
+require "poker_eval"
+t = PokerEval.new       
+# x = t.eval({"game"=>"holdem", "pockets"=>[["Qc","Jh"],["9h","6c"],["4c","4h"],["6h","8c"]], "board"=>["Ac","Kc"],  "iterations" => 1000})
+x = t.eval({"game"=>"holdem", "pockets"=>[["tc", "ac"],  ["th", "ah"],  ["8c", "6h"]], "board"=>["7h",
+        "3s", "2c", "7s", "7d"],  "iterations" => 10000})
+print x["info"]
+print "\n"
+for i in x["eval"]
+  print i
+  print "\n"
+end
+
