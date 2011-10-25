b3720c9d7cd15f823534d44fefc7d21570b9f227
miks <miks@cubesystems.lv>
2011-10-25 08:48:47 +0800

    Delete unneeded file

--- test.rb
+++
@@ -1,9 +0,0 @@
-require "./pokereval"
-t = PokerEval.new       
-x = t.eval({"game"=>"holdem", "pockets"=>[["Qc","Jh"],["9h","6c"],["4c","4h"],["6h","8c"]], "board"=>["Ac","Kc"],  "iterations" => 1000})
-print x["info"]
-print "\n"
-for i in x["eval"]
-  print i
-  print "\n"
-end
