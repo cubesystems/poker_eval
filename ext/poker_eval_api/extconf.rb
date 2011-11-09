5ea200aa4eff192478f6aefe3f9af03647c208a5
miks <miks@cubesystems.lv>
2011-11-09 22:48:38 +0200

    FreeBSD poker-eval lib finding problem solved

--- ext/poker_eval_api/extconf.rb
+++ ext/poker_eval_api/extconf.rb
@@ -1,7 +1,3 @@
 require 'mkmf'
-have_library("poker-eval")
 find_header('poker_defs.h', '/usr/local/include/poker-eval')
-# find_header('enumerate.h', '/usr/local/include/poker-eval')
-# find_header('enumdefs.h', '/usr/local/include/poker-eval')
-# dir_config("pokereval")
 create_makefile("poker_eval_api")
